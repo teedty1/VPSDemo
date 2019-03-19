03dcffcd1f779f43406b4093c700267402feef10
teedty1 <andy.castillo@sturgiswebservices.com>
2019-03-19 04:41:14 -0400

    Add Kraken and prep

---
+++ vps.zoid.js
@@ -0,0 +1,19 @@
+﻿
+window.VPSComponent = zoid.create({
+
+    // The html tag used to render my component
+    tag: 'vps-component',
+
+    // The url that will be loaded in the iframe or popup, when someone includes my component on their page
+    url: '/1903181417843D13E354319031814178VpsCheckout?token=1abc&callbackUrl=https://devel.vpsenv.com/Vps.CheckoutAPI.Version2/Payment/CurrentSession/1903181417843D13E354319031814178',
+
+    // Allow the component to be rendered as a popup
+    contexts: {
+        popup: true
+    },
+
+    dimensions: {
+        width: '500px',
+        height: '550px'
+    }
+});
