fd38ae63886d9b16eb43b653448bd97158d4b5f4
Andy Castillo <andy.castillo@sturgiswebservices.com>
2019-03-28 13:03:24 -0400

    more updates

--- vps.zoid.js
+++ vps.zoid.js
@@ -21,7 +21,7 @@ window.VPSComponent = zoid.create({
 window.VPSComponentv2 = zoid.create({
 
     // The html tag used to render my component
-    tag: 'vps-component',
+    tag: 'vps-componentv2',
 
     // The url that will be loaded in the iframe or popup, when someone includes my component on their page
     url: '/demos/Version2/',
