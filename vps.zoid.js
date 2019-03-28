03e50a6eb7b827fd08185e2795fc238f637dd3ce
Andy Castillo <andy.castillo@sturgiswebservices.com>
2019-03-28 13:01:13 -0400

    Updates for version 2

--- vps.zoid.js
+++ vps.zoid.js
@@ -17,3 +17,22 @@ window.VPSComponent = zoid.create({
         height: '550px'
     }
 });
+
+window.VPSComponentv2 = zoid.create({
+
+    // The html tag used to render my component
+    tag: 'vps-component',
+
+    // The url that will be loaded in the iframe or popup, when someone includes my component on their page
+    url: '/demos/Version2/',
+
+    // Allow the component to be rendered as a popup
+    contexts: {
+        popup: true
+    },
+
+    dimensions: {
+        width: '700px',
+        height: '550px'
+    }
+});
