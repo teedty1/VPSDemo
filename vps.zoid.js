b09f7de6bd4a191aa62267ab51a99c82d518e1f1
Andy Castillo <andy.castillo@sturgiswebservices.com>
2019-06-03 11:30:02 -0400

    Update vps.zoid.js

--- vps.zoid.js
+++ vps.zoid.js
@@ -5,7 +5,7 @@ window.VPSComponent = zoid.create({
     tag: 'vps-component',
 
     // The url that will be loaded in the iframe or popup, when someone includes my component on their page
-    url: '/Vps.CheckoutAPI.Version2/Payment/CurrentSession/1903181417843D13E354319031814178VpsCheckout?token=1abc&callbackUrl=https://devel.vpsenv.com/Vps.CheckoutAPI.Version2/Payment/CurrentSession/1903181417843D13E354319031814178',
+    url: '/Vps.CheckoutAPI.Version2/Payment/CurrentSession/1903181417843D13E354319031814178 ',
 
     // Allow the component to be rendered as a popup
     contexts: {
