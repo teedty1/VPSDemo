1fc7611722bd1f2ac7e0899b6acbcc72014664be
Andy Castillo <andy.castillo@sturgiswebservices.com>
2019-06-03 11:40:34 -0400

    fixing URLs

--- vps.zoid.js
+++ vps.zoid.js
@@ -5,26 +5,7 @@ window.VPSComponent = zoid.create({
     tag: 'vps-component',
 
     // The url that will be loaded in the iframe or popup, when someone includes my component on their page
-    url: '/Vps.CheckoutAPI.Version2/Payment/CurrentSession/1903181417843D13E354319031814178 ',
-
-    // Allow the component to be rendered as a popup
-    contexts: {
-        popup: true
-    },
-
-    dimensions: {
-        width: '700px',
-        height: '550px'
-    }
-});
-
-window.VPSComponentv2 = zoid.create({
-
-    // The html tag used to render my component
-    tag: 'vps-componentv2',
-
-    // The url that will be loaded in the iframe or popup, when someone includes my component on their page
-    url: '/demos/Version2/',
+    url: '/Vps.CheckoutAPI.Version2/Payment/CurrentSession/1903181417843D13E354319031814178',
 
     // Allow the component to be rendered as a popup
     contexts: {
